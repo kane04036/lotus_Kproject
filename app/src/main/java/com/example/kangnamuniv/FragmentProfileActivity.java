ebeb4cb91006b5cacc34e3d200db55df80352a3a
Chaewon Lee <78412766+Chaewon3Lee@users.noreply.github.com>
2022-05-07 01:21:59 +0900

    사용자정보 화면 (과목 변경)

--- app/src/main/java/com/example/kangnamuniv/FragmentProfileActivity.java
+++ app/src/main/java/com/example/kangnamuniv/FragmentProfileActivity.java
@@ -2,12 +2,14 @@ package com.example.kangnamuniv;
 
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +25,7 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class FragmentProfileActivity extends Fragment {
 
@@ -31,9 +34,22 @@ public class FragmentProfileActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        Button btnLogout;
+        TextView btnLogout;
         btnLogout = view.findViewById(R.id.btnLogout);
         String URL = "http://34.64.49.11/logout";//각 상황에 맞는 서버 url
+
+
+
+        TextView TextView1 = (TextView) view.findViewById(R.id.EditText01);
+
+        TextView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),MyInfoCheckActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
